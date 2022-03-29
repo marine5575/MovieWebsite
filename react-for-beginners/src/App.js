b1c2eedb53e5ce68df351ba4c6909b72e41f3c93
marine5575 <hope_insnow8@naver.com>
2022-03-29 17:55:03 +0900

    ~6.1 useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,20 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const onClick = () => setCounter((prev) => prev + 1);
+
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
